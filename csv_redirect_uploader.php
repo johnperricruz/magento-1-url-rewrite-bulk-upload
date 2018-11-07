151882e2c767b25a85a5f1a11a765086e731fbd6
John Perri Cruz <johnperri.cruz@acidgreen.com.au>
2018-11-07 08:48:44 +0800

    Initial Upload

---
+++ csv_redirect_uploader.php
@@ -0,0 +1,106 @@
+<?php 
+
+	/**
+	 * Tested on Magento 1.9
+	 *
+	 */
+
+	set_time_limit(0);
+
+	ini_set('memory_limit', '4G');
+
+	$mage = dirname(dirname(__DIR__)) . '/trunk/app/Mage.php'; //Mage location
+
+	if ( file_exists($mage) ){
+
+		require_once $mage;
+	    umask(0);
+	    Mage::init();
+
+		/**
+		 * RUN BELOW COMMAND ON TERMINAL
+		 * php {path_to_script}csv_redirect_uploader.php
+		 *
+		 */
+
+		$filename = 'import/redirect.csv'; //csv location
+
+		if (!file_exists($filename)) {
+   			echo PHP_EOL.'"'.$filename.'" does not exist! Aborting...'.PHP_EOL.PHP_EOL;
+   			exit;
+        }
+
+        $total = 0;
+        $totalSuccess = 0;
+        $totalUpdates = 0;
+        $length =  0;
+        $delimiter = ',';
+        $enclosure = '"';
+        $escape = '\\';
+        $skipline = 1; //Skip line 1
+
+       if (($fp = fopen($filename, 'r'))) {
+            while (($line = fgetcsv($fp, $length, $delimiter, $enclosure, $escape))) {
+
+                $total++;
+                if ($skipline && ($total == 1)) {
+                    continue;
+                }
+
+                $requestPath = $line[0];
+                $targetPath = $line[1];
+
+                $rewrite = Mage::getModel('core/url_rewrite');
+
+                /**
+                 * Dev note : This script will update duplicated request_path;
+                 *
+                 */
+
+                foreach($rewrite->getCollection() as $existing){
+                	if($existing['request_path'] == $requestPath){
+                		$existing->delete();
+                		$totalUpdates++;
+                	}
+                }
+
+           		$rewrite->setIdPath(uniqid())
+                    ->setTargetPath($targetPath)
+                    ->setOptions('RP')
+                    ->setDescription('Uploaded via script.')
+                    ->setRequestPath($requestPath)
+                    ->setIsSystem(0)
+                    ->setStoreId(0);
+
+                try {
+                   $rewrite->save();
+                   $totalSuccess++;
+                } catch (Exception $e) {
+                    $logException = $e->getMessage();
+                    Mage::logException($e);
+                }
+            }
+            fclose($fp);       
+             //Uncomment below if you want to delete file after upload.
+             //unlink($filename);
+
+           
+           	if($totalUpdates > 0){
+           		echo PHP_EOL.''.$totalSuccess - $totalUpdates.' URL rewrites have been imported.'.PHP_EOL;
+           		echo $totalUpdates.' URL rewrites have been updated.'.PHP_EOL;
+           	}else{
+           		echo PHP_EOL.''.$totalSuccess.' URL rewrites have been imported.'.PHP_EOL;
+           	}
+
+            if (!empty($logException)) {
+                echo 'Error : '.$logException.''.PHP_EOL.PHP_EOL;
+            }else{
+            	echo "Finished......100%".PHP_EOL.PHP_EOL;
+            }    	
+
+       }
+
+
+	}
+
+?>
